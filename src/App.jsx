5c8cc70e3350776acab4e503c198c99047812bc5
Mnzavasabby <rashidsabitina@gmail.com>
2024-05-07 12:37:43 +0300

    adding about component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './componets/Navbar/Navbar'
 import Hero from './componets/Hero/Hero'
 import Menu from './componets/Menus/Menu'
 import Title from './componets/Title/Title'
+import About from './componets/About/About'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <div className="container">
           <Title subTitle='Our Menu' title='What We Offer'/>
           <Menu/>
+          <About/>
       </div>
       
     </div>
---
+++ src/componets/About/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './About.css'
+import about_img from '../../assets/fried-meat.jpg'
+
+const About = () => {
+  return (
+    <div className='about'>
+        <div className="about-left">
+            <img src={about_img} alt="" />
+            <img src="" alt="" />
+        </div>
+        <div className="about-right">
+            <h3></h3>
+        </div>
+      
+    </div>
+  )
+}
+
+export default About
